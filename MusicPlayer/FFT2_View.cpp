0d8a2dffba1167c0c44efeea05124584125f046b
wws <1274704958@qq.com>
2018-04-17 11:05:01 +0800

    ...

--- MusicPlayer/FFT2_View.cpp
+++ MusicPlayer/FFT2_View.cpp
@@ -8,7 +8,7 @@ fv::FFT2_View::FFT2_View(float * pdata, int size,float w,float h) : FFT_View(pda
 	m_h = h;
 	m_unit_w =  w / size;
 	m_x = -(w / 2.f);
-	m_y = h + 200.f;
+	m_y = h / 2;
 	init();
 }
 
@@ -17,9 +17,9 @@ void fv::FFT2_View::init()
 
 	this->setFillColor(sf::Color(0, 0, 0, 0));
 	this->setOutlineColor(sf::Color(0, 255, 255));
-	this->setOutlineThickness(3.0);
+	this->setOutlineThickness(1.6);
 
-	for (int i = 0; i < m_fft_size; i++)
+	for (int i = 0; i < m_fft_size; ++i)
 	{
 		m_ps.push_back(sf::Vector2f(m_x + (i * m_unit_w) , m_y));
 		m_ps_copy.push_back(sf::Vector2f(m_x + (i * m_unit_w) , m_y));
@@ -27,20 +27,38 @@ void fv::FFT2_View::init()
 		m_ps.push_back(sf::Vector2f(m_x + (i * m_unit_w) + m_unit_w, m_y));
 		m_ps_copy.push_back(sf::Vector2f(m_x + (i * m_unit_w)  + m_unit_w, m_y));
 	}
-	m_ps.push_back(sf::Vector2f(m_x + m_fft_size * m_unit_w , m_y + m_h));
-	m_ps.push_back(sf::Vector2f(m_x, m_y + m_h));
+
+	for (int i = m_fft_size - 1; i >= 0; --i)
+	{
+		m_ps.push_back(sf::Vector2f(m_x + (i * m_unit_w) + m_unit_w, m_y - m_h));
+		m_ps_copy.push_back(sf::Vector2f(m_x + (i * m_unit_w) + m_unit_w, m_y - m_h));
+		
+		m_ps.push_back(sf::Vector2f(m_x + (i * m_unit_w), m_y - m_h));
+		m_ps_copy.push_back(sf::Vector2f(m_x + (i * m_unit_w), m_y - m_h));
+	}
+	update();
 }
 
 void fv::FFT2_View::updatePoint(float l, float h)
 {
+	int s = m_fft_size * 4 - 1;
 	for (int i = 0; i < m_fft_size; i++)
 	{
 		sf::Transform trans;
-		//printf("%f", sqrtf(pdata[i]));
-		trans.scale(1.0f , 1.0f-(sqrtf(m_pdata[i]) * 4.20f) );
+		//printf("%f\n", -(sqrtf(m_pdata[i]) * h * 100.0f));
+		trans.scale(1.0f , (sqrtf(m_pdata[i]) * h * 30.f) );
 
 		m_ps[i * 2] = trans.transformPoint(m_ps_copy[i * 2]);
 		m_ps[i * 2 + 1] = trans.transformPoint(m_ps_copy[i * 2 + 1]);
+
+		sf::Transform trans2;
+		//printf("%f\n", (sqrtf(m_pdata[i]) * l * 100.0f) );
+		trans2.scale(1.0f, (sqrtf(m_pdata[i]) * l * 30.0f) );
+
+		m_ps[s] = trans2.transformPoint(m_ps_copy[s]);
+		m_ps[s - 1] = trans2.transformPoint(m_ps_copy[s - 1]);
+
+		s -= 2;
 	}
 	update();
 }
